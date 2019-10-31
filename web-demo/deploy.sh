d647e061b2491d2539ed63572fbe9eb25eea401c
root <root@master-bin-172.dc.tw>
2019-11-01 07:58:25 +0800

    update deploy.sh

--- web-demo/deploy.sh
+++ web-demo/deploy.sh
@@ -9,9 +9,11 @@ echo "Deploying... name:${name}, image:${image}, host: ${HOST}"
 rm -rf web.yaml
 cp $(dirname "${BASH_SOURCE[0]}")/template/web.yaml .
 
+echo "copy success"
 sed -i "s,{{name}},${name},g" web.yaml
 sed -i "s,{{image}},${image},g" web.yaml
 sed -i "s,{{host}},${host},g" web.yaml
-
+echo "ready to apply"
 kubectl apply -f web.yaml
+echo "apply ok"
 cat web.yaml
