a72ba6e5ba383da852db83b08147202d307abe86
root <root@master-bin-172.dc.tw>
2019-11-01 08:08:10 +0800

    update deploy.sh

--- web-demo/deploy.sh
+++ web-demo/deploy.sh
@@ -7,8 +7,8 @@ host=${HOST}
 echo "Deploying... name:${name}, image:${image}, host: ${HOST}"
 
 rm -rf web.yaml
-cp $(dirname "${BASH_SOURCE[0]}")/template/web.yaml .
-
+#cp $(dirname "${BASH_SOURCE[0]}")/template/web.yaml .
+cp ${WORKSPACE}/template/web.yaml .
 echo "copy success"
 sed -i "s,{{name}},${name},g" web.yaml
 sed -i "s,{{image}},${image},g" web.yaml
